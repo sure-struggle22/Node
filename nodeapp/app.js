087e479c2f0546a4bce818e6a77f0bc85fab2b8a
sure-struggle22 <dwayneboy22995@gmail.com>
2021-05-12 23:09:55 +0530

    API endpoints

---
+++ nodeapp/app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const request = require('request');
+// const { query } = require('express');
+const server = http.createServer(function (req, res) {
+    if (req.url === ('/hello') && req.method === ('GET')) {
+        res.write('Hello from the ' + req.method);
+        res.end();
+
+    }
+    else if (req.url === ('/hello') && req.method === ('POST')) {
+        res.write('Hello from the ' + req.method);
+        res.end();
+    }
+    else if (req.url === '/me', req.method === ('GET')) {
+
+        const parsed = url.parse(req.url);
+        const query = querystring.parse(parsed.query);
+        // querystring.unescape(queryObject);
+        console.log(parsed);
+        console.log(query);
+
+
+
+        // res.write(queryObject);
+
+        const req1 = query.name;
+        res.write('hello ' + req1);
+        res.end();
+    }
+
+});
+
+server.listen(3000);
